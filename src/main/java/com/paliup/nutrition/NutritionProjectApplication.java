1b87b949f046b112d95cb5d07fb3331ae01e70b4
SariSabouh <sari.sbouh@gmail.com>
2016-10-28 14:18:15 -0500

    Initial Commit

---
+++ src/main/java/com/paliup/nutrition/NutritionProjectApplication.java
@@ -0,0 +1,12 @@
+package com.paliup.nutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionProjectApplication.class, args);
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main/js/main.jsx',
+    devtool: 'sourcemaps',
+    cache: true,
+    debug: true,
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
