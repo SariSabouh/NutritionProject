407e908fe338aa047af6419ac1682424519f1fbd
SariSabouh <sari.sbouh@gmail.com>
2017-01-06 14:36:18 -0600

    Revert "yes commit(maysara)"
    This reverts commit f3181813864148de1057321774e0363b70f9f10b.

--- src/main/java/com/paliup/nutrition/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.paliup.nutrition.service;
-import com.paliup.nutrition.Model.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByEmail(String username);
-}
-
