01bad17dce4e628c23f9e121ec6612f880160ff7
Maysaraodeh <maysaraodeh@192.168.1.112>
2017-01-25 21:44:13 -0800

    logo

--- src/main/js/main.jsx
+++ src/main/js/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import Login from'./components/Login/login.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+ReactDom.render(<Login />,document.getElementById('Login'));
